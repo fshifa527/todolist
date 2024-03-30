f16460f7cf28e9fc2ad2125187f56ed674d73fde
fshifa527 <fathimashifa.m27@gmail.com>
2024-03-30 15:52:08 +0530

    'first'

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import ToDoList from './components/ToDoList'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <ToDoList/>
+  )
+}
+
+export default App
